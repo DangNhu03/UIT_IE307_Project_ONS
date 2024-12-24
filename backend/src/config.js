27f0a18f0e7a4f94a2e28dd4a92500426d2440c7
Lamcam <lamcam813@gmail.com>
2024-12-24 11:25:39 +0700

    Update payment

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,8 +3,8 @@ module.exports = {
   secretKey: "K951B6PE1waDMi640xX08PD3vg6EkVlz",
   orderInfo: "pay with MoMo",
   partnerCode: "MOMO",
-  redirectUrl: "exp://172.17.11.160:8081/--/home --android",
-  ipnUrl: "https://6ae0-113-161-91-25.ngrok-free.app/orders/callback", //chú ý: cần dùng ngrok thì momo mới post đến url này được
+  redirectUrl: "exp://172.17.17.82:8081/--/home --android",
+  ipnUrl: "https://53df-113-161-91-204.ngrok-free.app/orders/callback", //chú ý: cần dùng ngrok thì momo mới post đến url này được
   requestType: "payWithMethod",
   extraData: "",
   orderGroupId: "",
